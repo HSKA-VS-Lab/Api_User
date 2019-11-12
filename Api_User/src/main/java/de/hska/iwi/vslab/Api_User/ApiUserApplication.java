ec9c10a73ec1918de6b9136a69824dbc9affeebf
and1kl <40032530+and1kl@users.noreply.github.com>
2019-11-12 18:33:56 +0100

    Spring Boot Project Initial

---
+++ Api_User/src/main/java/de/hska/iwi/vslab/Api_User/ApiUserApplication.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Api_User;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserApplication.class, args);
+	}
+
+}
---
+++ Api_User/src/test/java/de/hska/iwi/vslab/Api_User/ApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Api_User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
