89b1ccd9e73c3260a09f19e8f882b7a40502c8cf
Marls2910 <marleen.eisenacht@gmx.de>
2020-01-22 12:42:26 +0100

    add fallbacks with cache

--- Api_User/src/main/java/de/hska/iwi/vslab/Api_User/Controllers/ApiUserController.java
+++ Api_User/src/main/java/de/hska/iwi/vslab/Api_User/Controllers/ApiUserController.java
@@ -1,10 +1,14 @@
 package de.hska.iwi.vslab.Api_User.Controllers;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import de.hska.iwi.vslab.Api_User.Services.ApiUserService;
 import de.hska.iwi.vslab.Api_User.ConsumingREST.User;
 
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,63 +19,86 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @EnableCircuitBreaker
 public class ApiUserController {
-
+    private final Map<Integer, User> userCache = new LinkedHashMap<Integer, User>();
     @Autowired
     private ApiUserService apiUserService;
 
     private static final Logger log = LoggerFactory.getLogger(ApiUserController.class);
 
     @GetMapping("/user")
-    @HystrixCommand(fallbackMethod = "getFallbackUsers")
+    @HystrixCommand(fallbackMethod = "getUsersCache", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2") })
     public User[] getAllUsers() {
-        return apiUserService.getAllUsers();
+        User[] users = apiUserService.getAllUsers();
+        for(User user : users) {
+            userCache.putIfAbsent(user.getId(), user);
+        }
+        return users;
     }
 
-    public User[] getFallbackUsers() {
-        User user1 = new User("userFallback1", "max", "mustermann","pw123", 0);
-        User user2 = new User("userFallback2", "marie", "musterfrau","pw123", 0);
-        User[] userA = new User[2];
-        userA[0] = user1;
-        userA[1] = user2;
-        return userA;
+    public User[] getUsersCache() {
+        Collection<User> list = userCache.values();
+        User[] users = new User[userCache.values().size()];
+        users = list.toArray(users);
+        return users;
     }
 
     @GetMapping("/user/{input}")
-    //@HystrixCommand(fallbackMethod = "fallbackGetUser")
+    @HystrixCommand(fallbackMethod = "getUserCache", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2") })
     public User getUser(@PathVariable String input) {
         return apiUserService.getUser(input);
     }
 
-    public User fallbackGetUser() {
-        User user = new User("userFallback1", "max", "mustermann","pw123", 0);
-        return user;
+    public User getUserCache(String input) {
+        System.out.println("UserFromCache: "+ userCache.values());
+        Collection<User> users = userCache.values();
+        if (input.replaceAll("\\d", "").length() > 0) { // only digits in input
+            User filteredUser = users.stream()
+                    .filter(user -> input.equals(user.getUsername()))
+                    .findAny()
+                    .orElse(new User());
+            System.out.println("UserFromCache: "+ filteredUser);
+            return filteredUser;
+        } else { // get by id
+            User filteredUser = users.stream()
+                    .filter(user -> Integer.parseInt(input) == user.getId())
+                    .findAny()
+                    .orElse(new User());
+            return filteredUser;
+        }
     }
 
     @PostMapping(path="/user", consumes="application/json")
-    //@HystrixCommand(fallbackMethod = "defaultFallback")
+    @HystrixCommand(fallbackMethod = "registerUserFallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2") })
     public void addUser(@RequestBody(required=true) User payload) {
-        log.info("register(firstname, lastname, username, password) was called. Payload:", payload.toString());
         apiUserService.register(payload.getFirstname(), payload.getLastname(), payload.getUsername(), payload.getPassword());
     }
 
+    public void registerUserFallback(User payload, Throwable e) {
+         System.out.printf("Register User Failed! username=%s, exception=%s%n ", payload.getUsername(), e);
+    }
+
     @PutMapping(path="/user/{id}", consumes="application/json")
-    //@HystrixCommand(fallbackMethod = "defaultFallbackWithId")
+    @HystrixCommand(fallbackMethod = "updateUserFallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2") })
     public void updateUser(@PathVariable int id,  @RequestBody(required=true) User payload) {
-        log.info("updateUser(roleId, firstname, lastname, username, password) was called. Payload:", payload);
         apiUserService.updateUser(id, payload.getFirstname(), payload.getLastname(), payload.getUsername(), payload.getPassword(), payload.getRoleId());
     }
 
+    public void updateUserFallback(int id, User payload, Throwable e) {
+        System.out.printf("Update User Failed! id=%s, exception=%s%n ", id, e);
+    }
+
     @DeleteMapping("/user/{id}")
-    //@HystrixCommand(fallbackMethod = "defaultFallbackWithId")
+    @HystrixCommand(fallbackMethod = "deleteUserFallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2") })
     public void deleteUser(@PathVariable int id){
         apiUserService.deleteUser(id);
     }
 
-    public void defaultFallback(Throwable throwable) {
-        System.out.printf("DefaultFallback, exception=%s%n", throwable);
-    }
-
-    public void defaultFallbackWithId(int id, Throwable throwable) {
-        System.out.printf("DefaultFallbackWithId, id=%s, exception=%s%n", id, throwable);
+    public void deleteUserFallback(int id, Throwable throwable) {
+        System.out.printf("Delete user failed, id=%s, exception=%s%n", id, throwable);
     }
 }
