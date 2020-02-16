5d781dde3aba01dc7ccc9492047faef725c5fb4d
BobFlop <BobFlop@users.noreply.github.com>
2020-02-16 12:53:11 +0100

    OAuth2 Api User (should work) however core_user doesn't want to work

--- Api_User/src/main/java/de/hska/iwi/vslab/Api_User/ConsumingREST/ConsumeCoreUser.java
+++ Api_User/src/main/java/de/hska/iwi/vslab/Api_User/ConsumingREST/ConsumeCoreUser.java
@@ -1,22 +1,54 @@
 package de.hska.iwi.vslab.Api_User.ConsumingREST;
 
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.test.OAuth2ContextConfiguration.ClientCredentials;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.web.client.RestTemplate;
 
-
+@EnableOAuth2Client
 public class ConsumeCoreUser {
 
     //private String urlCoreUser = "http://localhost:8083/user";
 
+    @Value("${oauth.resource:http://localhost:8082}")
+    String bla;
+
+    @Value("${security.oauth2.client.client-id}")
+    String clientId;
+    @Value("${security.oauth2.client.client-secret}")
+    String clientSecret;
+
+    @Autowired
+    OAuth2RestTemplate restTemplate2;
+
     private static final Logger log = LoggerFactory.getLogger(ConsumeCoreUser.class);
     RestTemplate restTemplate = new RestTemplate();
+
+    @Bean
+    public static OAuth2RestTemplate  foo(OAuth2ProtectedResourceDetails details) {
+        OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(details);
+
+        /* To validate if required configurations are in place during startup */
+        oAuth2RestTemplate.getAccessToken();
+        return oAuth2RestTemplate;
+    }
 
     public User[] getAllUsers() {
         try {
             UrlBuilder urlBuilder = new UrlBuilder();
             log.info("URL:" + urlBuilder.getBaseUrl_core_user());
-            return restTemplate.getForObject(urlBuilder.getUserUrl(), User[].class);
+            return restTemplate2.getForObject(urlBuilder.getUserUrl(), User[].class);
         } catch (Exception e) {
             System.out.println(e);
             throw e;
